9ce7c104b4a83040fe542fdecc82547be5a44ab0
Rathijit Paul <alienpapon009@gmail.com>
2023-02-27 15:46:52 +0600

    :fire: Remove Toastify dependencies Configuration due to package update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import "react-toastify/dist/ReactToastify.css";
 import 'reactjs-popup/dist/index.css';
-import {toast} from "react-toastify";
 import App from './App';
-
-toast.configure();
 
 ReactDOM.render(
   <App />,
